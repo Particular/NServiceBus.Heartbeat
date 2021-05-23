5d9112496edc8d9e760ea675bc178fd21f206b11
Adam Ralph <adam@adamralph.com>
2021-05-23 13:09:20 +0200

    fix up OCE catching and Task.Run(), etc.

--- src/NServiceBus.Heartbeat/HeartbeatSender.cs
+++ src/NServiceBus.Heartbeat/HeartbeatSender.cs
@@ -30,42 +30,45 @@
             stopSendingHeartbeatsTokenSource = new CancellationTokenSource();
 
             // don't block here since StartupTasks are executed synchronously.
-            _ = SendEndpointStartupMessage(DateTime.UtcNow, stopSendingHeartbeatsTokenSource.Token);
+            _ = SendEndpointStartupMessageAndSwallowExceptions(DateTime.UtcNow, stopSendingHeartbeatsTokenSource.Token);
 
             Logger.Debug($"Start sending heartbeats every {heartbeatInterval}");
 
             _ = Task.Run(async () =>
                 {
-                    while (!stopSendingHeartbeatsTokenSource.IsCancellationRequested)
+                    try
                     {
-                        try
+                        while (true)
                         {
-                            await Task.Delay(heartbeatInterval, stopSendingHeartbeatsTokenSource.Token).ConfigureAwait(false);
-
-                            var message = new EndpointHeartbeat
+                            try
                             {
-                                ExecutedAt = DateTime.UtcNow,
-                                EndpointName = endpointName,
-                                Host = hostInfo.DisplayName,
-                                HostId = hostInfo.HostId
-                            };
+                                await Task.Delay(heartbeatInterval, stopSendingHeartbeatsTokenSource.Token).ConfigureAwait(false);
 
-                            await backend.Send(message, ttlTimeSpan, dispatcher, stopSendingHeartbeatsTokenSource.Token).ConfigureAwait(false);
-                        }
-                        catch (OperationCanceledException ex)
-                        {
-                            if (stopSendingHeartbeatsTokenSource.IsCancellationRequested)
-                            {
-                                Logger.Debug("Heartbeat sending cancelled.", ex);
+                                var message = new EndpointHeartbeat
+                                {
+                                    ExecutedAt = DateTime.UtcNow,
+                                    EndpointName = endpointName,
+                                    Host = hostInfo.DisplayName,
+                                    HostId = hostInfo.HostId
+                                };
+
+                                await backend.Send(message, ttlTimeSpan, dispatcher, stopSendingHeartbeatsTokenSource.Token).ConfigureAwait(false);
                             }
-                            else
+                            catch (Exception ex) when (!(ex is OperationCanceledException))
                             {
-                                Logger.Warn("OperationCanceledException thrown.", ex);
+                                Logger.Warn("Unable to send heartbeat to ServiceControl.", ex);
                             }
                         }
-                        catch (Exception ex)
+                    }
+                    catch (OperationCanceledException ex)
+                    {
+                        if (stopSendingHeartbeatsTokenSource.IsCancellationRequested)
                         {
-                            Logger.Warn("Unable to send heartbeat to ServiceControl.", ex);
+                            Logger.Debug("Heartbeat sending canceled.", ex);
+                        }
+                        else
+                        {
+                            Logger.Warn("OperationCanceledException thrown.", ex);
                         }
                     }
                 },
@@ -81,46 +84,49 @@
             return Task.CompletedTask;
         }
 
-        async Task SendEndpointStartupMessage(DateTime startupTime, CancellationToken cancellationToken)
+        async Task SendEndpointStartupMessageAndSwallowExceptions(DateTime startupTime, CancellationToken stopSendingHeartbeatsCancellationToken)
         {
             try
             {
-                var message = new RegisterEndpointStartup
+                try
                 {
-                    HostId = hostInfo.HostId,
-                    Host = hostInfo.DisplayName,
-                    Endpoint = endpointName,
-                    HostDisplayName = hostInfo.DisplayName,
-                    HostProperties = hostInfo.Properties,
-                    StartedAt = startupTime
-                };
-                await backend.Send(message, ttlTimeSpan, dispatcher, cancellationToken).ConfigureAwait(false);
+                    var message = new RegisterEndpointStartup
+                    {
+                        HostId = hostInfo.HostId,
+                        Host = hostInfo.DisplayName,
+                        Endpoint = endpointName,
+                        HostDisplayName = hostInfo.DisplayName,
+                        HostProperties = hostInfo.Properties,
+                        StartedAt = startupTime
+                    };
+                    await backend.Send(message, ttlTimeSpan, dispatcher, stopSendingHeartbeatsCancellationToken).ConfigureAwait(false);
+                }
+                catch (Exception ex) when (!(ex is OperationCanceledException))
+                {
+                    if (!resendRegistration)
+                    {
+                        Logger.Warn("Unable to register endpoint startup with ServiceControl.", ex);
+                        return;
+                    }
+
+                    resendRegistration = false;
+
+                    Logger.Warn($"Unable to register endpoint startup with ServiceControl. Going to reattempt registration after {registrationRetryInterval}.", ex);
+
+                    await Task.Delay(registrationRetryInterval, stopSendingHeartbeatsCancellationToken).ConfigureAwait(false);
+                    await SendEndpointStartupMessageAndSwallowExceptions(startupTime, stopSendingHeartbeatsCancellationToken).ConfigureAwait(false);
+                }
             }
             catch (OperationCanceledException ex)
             {
                 if (stopSendingHeartbeatsTokenSource.IsCancellationRequested)
                 {
-                    Logger.Debug("Heartbeat sending cancelled.", ex);
+                    Logger.Debug("Heartbeat sending canceled.", ex);
                 }
                 else
                 {
                     Logger.Warn("OperationCanceledException thrown.", ex);
                 }
-            }
-            catch (Exception ex)
-            {
-                if (!resendRegistration)
-                {
-                    Logger.Warn("Unable to register endpoint startup with ServiceControl.", ex);
-                    return;
-                }
-
-                resendRegistration = false;
-
-                Logger.Warn($"Unable to register endpoint startup with ServiceControl. Going to reattempt registration after {registrationRetryInterval}.", ex);
-
-                await Task.Delay(registrationRetryInterval, cancellationToken).ConfigureAwait(false);
-                await SendEndpointStartupMessage(startupTime, cancellationToken).ConfigureAwait(false);
             }
         }
 
