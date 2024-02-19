4c47d77a1b998b662f4a45b3abb9ca052ac492ff
Andreas Öhlund <andreas.ohlund@particular.net>
2024-02-19 09:03:18 +0100

    Guards

--- src/NServiceBus.Heartbeat/HeartbeatConfigurationExtensions.cs
+++ src/NServiceBus.Heartbeat/HeartbeatConfigurationExtensions.cs
@@ -18,16 +18,23 @@
         /// <param name="timeToLive">The maximum time to live for the heartbeat.</param>
         public static void SendHeartbeatTo(this EndpointConfiguration config, string serviceControlQueue, TimeSpan? frequency = null, TimeSpan? timeToLive = null)
         {
-            config.EnableFeature<HeartbeatsFeature>();
+            ArgumentNullException.ThrowIfNull(config);
+            ArgumentException.ThrowIfNullOrEmpty(serviceControlQueue);
+
             config.GetSettings().Set("NServiceBus.Heartbeat.Queue", serviceControlQueue);
-            if (timeToLive.HasValue)
-            {
-                config.GetSettings().Set("NServiceBus.Heartbeat.Ttl", timeToLive.Value);
-            }
             if (frequency.HasValue)
             {
+                ArgumentOutOfRangeException.ThrowIfLessThan(frequency.Value, TimeSpan.Zero);
                 config.GetSettings().Set("NServiceBus.Heartbeat.Interval", frequency.Value);
             }
+
+            if (timeToLive.HasValue)
+            {
+                ArgumentOutOfRangeException.ThrowIfLessThan(timeToLive.Value, TimeSpan.Zero);
+                config.GetSettings().Set("NServiceBus.Heartbeat.Ttl", timeToLive.Value);
+            }
+
+            config.EnableFeature<HeartbeatsFeature>();
         }
     }
 }
